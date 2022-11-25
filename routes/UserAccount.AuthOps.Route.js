fab6c53ef6a74930dc2c0120785c567c42e0f9cc
erentrk01 <erentarak1907@gmail.com>
2022-11-25 20:52:03 +0300

    registerUser endpoint done

--- routes/UserAccount.AuthOps.Route.js
+++ routes/UserAccount.AuthOps.Route.js
@@ -10,7 +10,7 @@ router.post("/", async (req, res,next) => {
 
 	// User Registration
 
-	const { buildingID,name, email, password} = req.body;
+	const { buildingId,name, email, password} = req.body;
 	// the creator of the building account is automatically assigned as a manager of the building account
 	const isManager = false;
 	const plaint_password = password;
@@ -19,23 +19,22 @@ router.post("/", async (req, res,next) => {
 	var salt = hash_data.salt;//save the salt
 	// check whether the user already exists
 
-	let user = User.findOne({email:email})
+	let user = await User.findOne({email:email})
 	// there is no user with the same email address
-		if(!user){
+		if(user){
 			return res.status(400).send("User already registered");
 			console.log("User already registered");
 		}
 	
-	
+	console.log("buildingID: "+buildingId);
 
-	 let building =Building.findOne({buildingId:buildingID});
-	 if(!building){
-		return res.status(400).send("Building account does not exist,register failed");
-	 }
+	 let building = await Building.findOne({buildingId:buildingId});
+	 if(!building) return res.status(400).send("Building account does not exist,register failed");
 
-	const save_user = await new User({buildingId,name, email, save_password,isManager,salt }).save();
 
-	res.status(200).send({buildingId:buildingId,user:save_user,building:save_building});
+	const save_user = await new User({buildingId:buildingId,name:name, email:email, password:save_password,isManager:isManager,salt:salt }).save();
+
+	res.status(200).send({buildingId:buildingId,user:save_user,building:building});
 
 	console.log("user registered successfully under a building account ");
 

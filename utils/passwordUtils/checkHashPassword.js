2378ceb1e40930b5c867fa6a7179b74403340095
erentrk01 <erentarak1907@gmail.com>
2022-11-25 08:49:59 +0300

    import fix

--- utils/passwordUtils/checkHashPassword.js
+++ utils/passwordUtils/checkHashPassword.js
@@ -1,3 +1,5 @@
+const sha512 = require("./sha512");
+
 checkHashPassword = (password, salt) => {
 	const passwordData = sha512(password, salt);
 	return passwordData;
--- utils/passwordUtils/saltHashPassword.js
+++ utils/passwordUtils/saltHashPassword.js
@@ -1,3 +1,6 @@
+const genRandomString =require("./genRandomString");
+const sha512 = require("./sha512");
+
 const saltHashPassword = (userPassword) => {
 	const salt = genRandomString(16); // Gives us salt of length 16
 	const passwordData = sha512(userPassword, salt);
